221529eb6546f042b01ce1ec152e0da5c50f5c7a
Thiago Gesteira <tcgest@Thiagos-MBP.attlocal.net>
2024-09-17 14:43:43 -0400

    Updated test.v and counter.v

--- verilog/hw3/Tutorial 1 (for Homework 3)-20240917/counter.v
+++ verilog/hw3/Tutorial 1 (for Homework 3)-20240917/counter.v
@@ -15,15 +15,16 @@
 
 /*======Declarations===============================*/
 
-module counter (clock, in, latch, dec,  zero);
+module counter (clock, in, latch, dec, divByTwo, zero);
 
 
 /*-----------Inputs--------------------------------*/
 
 input       clock;  /* clock */
-input [3:0] in;  /* input initial count */
+input [7:0] in;  /* input initial count */
 input       latch;  /* `latch input' */
 input       dec;   /* decrement */
+input       divByTwo;  /* divide by two */
 
 /*-----------Outputs--------------------------------*/
 
@@ -32,14 +33,28 @@ output      zero;  /* zero flag */
 /*----------------Nets and Registers----------------*/
 /*---(See input and output for unexplained variables)---*/
 
-reg [3:0] value;       /* current count value */
+reg [7:0] value;       /* current count value */
 wire      zero;
 
 // Count Flip-flops with input multiplexor
+
+/*
 always@(posedge clock)
   begin  // begin-end not actually need here as there is only one statement
     if (latch) value <= in;
-    else if (dec && !zero) value <= value - 1'b1;  
+    else if (dec && !zero) value <= value - 1'b1;
+    else if (!dec && !zero && divByTwo) value <= value >> 1;
+  end
+*/
+
+always@(posedge clock)
+  begin  // begin-end not actually need here as there is only one statement
+    casex ({latch, dec, zero, divByTwo}) 
+      4'b1???: value <= in;
+      4'b010?: value <= value - 1'b1;
+      4'b0001: value <= value >> 1;
+      default: value <= value;
+    endcase
   end
 
 // combinational logic for zero flag
--- verilog/hw3/Tutorial 1 (for Homework 3)-20240917/test.v
+++ verilog/hw3/Tutorial 1 (for Homework 3)-20240917/test.v
@@ -1,8 +1,9 @@
 module test_fixture;
 	reg		clock100 = 0 ;
 	reg		latch = 0;
-	reg 	dec = 0;
-	reg		[3:0] in = 4'b0101;
+	reg		dec = 0;
+	reg		[7:0] in = 8'b01010101;
+	reg		divByTwo = 0;
 	wire	zero;   
 	
 	initial	//following block executed only once
@@ -12,12 +13,15 @@ module test_fixture;
 		#16 latch = 1;		// wait 16 ns
 		#10 latch = 0;		// wait 10 ns
 		#10 dec = 1;
+		#9 dec = 0;
+		#11 divByTwo = 1;
+		#10 divByTwo = 0;
 		#100 $finish;		//finished with simulation
   	end
 	always #5 clock100 = ~clock100;	// 10ns clock
 
 	// instantiate modules -- call this counter u1
-	 counter u1(  .clock(clock100), .in(in), .latch(latch), .dec(dec), 	 		
-	 			  .zero(zero));
+	 counter u1(  .clock(clock100), .in(in), .latch(latch), .dec(dec),
+					.divByTwo(divByTwo), .zero(zero));
 endmodule  /*test_fixture*/
 
